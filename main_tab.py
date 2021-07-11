6ed3de330ee3196d576385130811ca8ccfb0b80b
noashmulevich <62446342+noashm@users.noreply.github.com>
2021-07-11 19:30:24 +0300

    updated

--- main_tab.py
+++ main_tab.py
@@ -7,23 +7,24 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import folium
 import dash_table as dt
-from pre_process import *
 
-df = pd.read_csv('gal_data.csv')
+df = pd.read_csv('df_main_dash.csv')
 
 app = dash.Dash(__name__)
+colors = {'title': 'skyblue'}
 
-app.layout = html.Div([
-    html.Div(
-    html.H1('Hadar Neighborhood: Semi-annual report'),
-    html.Iframe(id='map', srcDoc=open('map.html', 'r').read(), width="90%", height="700")
-    ),
+app.layout = html.Div(children=[
+    html.H1(children ='Hadar Neighborhood: Semi-annual report',
+    style={'text-align':'center', 'fontSize': 100,'font-family':'sans-serif'
+        , 'color':colors['title']}),
+    html.Iframe(id='map', srcDoc=open('map.html', 'r').read(), width="70%", height="700",
+                style={'justify-content': 'center'}),
+
     dt.DataTable(id="table",
     columns = [{"name": i, "id": i} for i in df.columns],
-              data = df.to_dict('records')
+              data=df.to_dict('records')
     )
     ])
-
 
 
 if __name__ == '__main__':
