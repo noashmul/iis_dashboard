65b9fdbc224120e23abdb399bd82fda745fdae02
ggoldste <gal.goldstein@intel.com>
2021-07-11 19:06:54 +0300

    add create_df_main_dash function

--- pre_process.py
+++ pre_process.py
@@ -15,64 +15,40 @@ import gdown
 import zipfile
 
 
-url = 'https://drive.google.com/uc?id=1-s1881dNo8ksiRsBa_V18eEr6I4ckTdf'
-path_to_zip_file = 'allfiles.zip'
-gdown.download(url, path_to_zip_file, quiet=False)
-
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall("data")
-
-df_crime_t0 = pd.read_csv('data/df_crime_t0.csv').drop(columns=['StatZone','Year'])
-df_crime_t1 = pd.read_csv('data/df_crime_t1.csv').drop(columns=['StatZone','Year'])
-df_crime_2010_to_2015 = pd.read_csv('data/df_crime_2010_to_2015.csv')
-df_conflicts_t0 = pd.read_csv('data/df_conflicts_t0.csv')
-df_conflicts_t1 = pd.read_csv('data/df_conflicts_t1.csv')
-
-df_salaries_t0 = pd.read_csv('data/df_salaries_t0.csv')
-df_salaries_t1 = pd.read_csv('data/df_salaries_t1.csv')
-df_haredim_t0 = pd.read_csv('data/df_haredim_t0.csv')
-df_haredim_t1 = pd.read_csv('data/df_haredim_t1.csv')
-
-df_seniors_t0 = pd.read_csv('data/df_seniors_t0.csv')
-df_seniors_t1 = pd.read_csv('data/df_seniors_t1.csv')
-df_holocaust_t0 = pd.read_csv('data/df_holocaust_t0.csv')
-df_holocaust_t1 = pd.read_csv('data/df_holocaust_t1.csv')
-
-df_main_dash = pd.DataFrame(columns=['Subject', 'Description', 'Value', 'Percent_comparison'])
-
 
 def percentage_change(a, b):
     return round(100 * (b - a) / a, 2)
 
 
-def add_row_to_main_dash(Subject: str, Description: str, t0_total, t1_total):
-    global df_main_dash
+def add_row_to_main_dash(df_main_dash, Subject: str, Description: str, t0_total, t1_total):
     crime_sample = pd.Series([Subject, Description,
                               t1_total, percentage_change(t0_total, t1_total)], index=df_main_dash.columns)
     df_main_dash = df_main_dash.append(crime_sample, ignore_index=True)
+    return df_main_dash
 
+def create_df_main_dash(d: dict):
+    df_main_dash = pd.DataFrame(columns=['Subject', 'Description', 'Value', 'Percent_comparison'])
 
-def create_df_main_dash():
     # Crime || Total Crimes
-    df_crime_t0['total_crimes'] = df_crime_t0.sum(axis=1)
-    df_crime_t1['total_crimes'] = df_crime_t1.sum(axis=1)
-    add_row_to_main_dash('Crime', 'Total Crimes in Current Semi-Year',
-                         df_crime_t0['total_crimes'].sum(), df_crime_t1['total_crimes'].sum())
+    d['df_crime_t0']['total_crimes'] = d['df_crime_t0'].sum(axis=1)
+    d['df_crime_t1']['total_crimes'] = d['df_crime_t1'].sum(axis=1)
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Crime', 'Total Crimes in Current Semi-Year',
+                         d['df_crime_t0']['total_crimes'].sum(), d['df_crime_t1']['total_crimes'].sum())
 
     # Population || Total Population
-    add_row_to_main_dash('Demographics', 'Total Population',
-                         int(df_salaries_t0['ResNum'].sum()), int(df_salaries_t1['ResNum'].sum()))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Demographics', 'Total Population',
+                         int(d['df_salaries_t0']['ResNum'].sum()), int(d['df_salaries_t1']['ResNum'].sum()))
 
     # Population || Total Haredim
-    add_row_to_main_dash('Demographics', 'Total Haredi Population',
-                         int(df_haredim_t0['TotHaredim'].sum()), int(df_haredim_t1['TotHaredim'].sum()))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Demographics', 'Total Haredi Population',
+                         int(d['df_haredim_t0']['TotHaredim'].sum()), int(d['df_haredim_t1']['TotHaredim'].sum()))
 
     # Income || Average Salary (Weighted sum of the five categories)
     Average_Salary_list = [0, 0]
 
-    for i, df_salaries in enumerate([df_salaries_t0, df_salaries_t1]):
-        df_salaries['ResNum_Salary'] = df_salaries['SalNoHKResNum'] + df_salaries['SalHKResNum'] + df_salaries[
-            'SalPenResNum'] + df_salaries['SalSHNoBTLResNum'] + df_salaries['IncSelfResNum']
+    for i, df_salaries in enumerate([d['df_salaries_t0'], d['df_salaries_t1']]):
+        df_salaries['ResNum_Salary'] = df_salaries['SalNoHKResNum'] + df_salaries['SalHKResNum'] + \
+        df_salaries['SalPenResNum'] + df_salaries['SalSHNoBTLResNum'] + df_salaries['IncSelfResNum']
         df_salaries['weighted_salary'] = df_salaries.apply(
             lambda x: int((x.SalNoHKResNum / x.ResNum_Salary) * x.SalNoHKAve +
                           (x.SalHKResNum / x.ResNum_Salary) * x.SalHKAve +
@@ -83,30 +59,63 @@ def create_df_main_dash():
         for ResNum, Salary in zip(df_salaries['ResNum_Salary'], df_salaries['weighted_salary']):
             Average_Salary_list[i] += (ResNum / (df_salaries['ResNum_Salary'].sum())) * Salary
 
-    add_row_to_main_dash('Demographics', 'Average Salary', int(Average_Salary_list[0]), int(Average_Salary_list[1]))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Demographics', 'Average Salary', int(Average_Salary_list[0]), int(Average_Salary_list[1]))
 
     # Elderly || Senior Citizens || len(df_seniors)
-    add_row_to_main_dash('Elderly', 'Number of Senior Citizens', len(df_seniors_t0), len(df_seniors_t0))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Elderly', 'Number of Senior Citizens', len(d['df_seniors_t0']), len(d['df_seniors_t0']))
 
     # Elderly || Lone Senior Citizens
-    add_row_to_main_dash('Elderly', 'Number of Lone Senior Citizens',
-                         df_seniors_t0['SeniorAlone'].sum(), df_seniors_t1['SeniorAlone'].sum())
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Elderly', 'Number of Lone Senior Citizens',
+                         d['df_seniors_t0']['SeniorAlone'].sum(), d['df_seniors_t1']['SeniorAlone'].sum())
 
     # Elderly || Senior Citizens That Recieve Food ||
-    add_row_to_main_dash('Elderly', 'Number of Recieving Food Senior Citizens',
-                         df_seniors_t0['SeniorRecivFood'].sum(), df_seniors_t1['SeniorRecivFood'].sum())
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Elderly', 'Number of Recieving Food Senior Citizens',
+                         d['df_seniors_t0']['SeniorRecivFood'].sum(), d['df_seniors_t1']['SeniorRecivFood'].sum())
 
     # Elderly || Holocaust Srvivors
-    add_row_to_main_dash('Elderly', 'Number of Holocaust Srvivors', len(df_holocaust_t0), len(df_holocaust_t1))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Elderly', 'Number of Holocaust Srvivors', len(d['df_holocaust_t0']), len(d['df_holocaust_t1']))
 
     # Elderly || Unrecognized Holocaust Survivors ||
-    add_row_to_main_dash('Elderly', 'Number of Unrecognized Holocaust Srvivors',
-                         sum(df_holocaust_t0['HoloSurvKnwn'] == "לא מוכר"),
-                         sum(df_holocaust_t1['HoloSurvKnwn'] == "לא מוכר"))
+    df_main_dash = add_row_to_main_dash(df_main_dash, 'Elderly', 'Number of Unrecognized Holocaust Srvivors',
+                         sum(d['df_holocaust_t0']['HoloSurvKnwn'] == "לא מוכר"),
+                         sum(d['df_holocaust_t1']['HoloSurvKnwn'] == "לא מוכר"))
+    return df_main_dash
 
 
 def main():
-    create_df_main_dash()
+    url = 'https://drive.google.com/uc?id=1-s1881dNo8ksiRsBa_V18eEr6I4ckTdf'
+    path_to_zip_file = 'allfiles.zip'
+    gdown.download(url, path_to_zip_file, quiet=False)
+
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall("data")
+
+    df_crime_t0 = pd.read_csv('data/df_crime_t0.csv').drop(columns=['StatZone', 'Year'])
+    df_crime_t1 = pd.read_csv('data/df_crime_t1.csv').drop(columns=['StatZone', 'Year'])
+    df_crime_2010_to_2015 = pd.read_csv('data/df_crime_2010_to_2015.csv')
+    df_conflicts_t0 = pd.read_csv('data/df_conflicts_t0.csv')
+    df_conflicts_t1 = pd.read_csv('data/df_conflicts_t1.csv')
+
+    df_salaries_t0 = pd.read_csv('data/df_salaries_t0.csv')
+    df_salaries_t1 = pd.read_csv('data/df_salaries_t1.csv')
+    df_haredim_t0 = pd.read_csv('data/df_haredim_t0.csv')
+    df_haredim_t1 = pd.read_csv('data/df_haredim_t1.csv')
+
+    df_seniors_t0 = pd.read_csv('data/df_seniors_t0.csv')
+    df_seniors_t1 = pd.read_csv('data/df_seniors_t1.csv')
+    df_holocaust_t0 = pd.read_csv('data/df_holocaust_t0.csv')
+    df_holocaust_t1 = pd.read_csv('data/df_holocaust_t1.csv')
+
+    dfs_dict = {'df_crime_t0': df_crime_t0, 'df_crime_t1': df_crime_t1,
+                'df_crime_2010_to_2015': df_crime_2010_to_2015,
+                'df_conflicts_t0': df_conflicts_t0, 'df_conflicts_t1': df_conflicts_t1,
+                'df_salaries_t0': df_salaries_t0, 'df_salaries_t1': df_salaries_t1,
+                'df_haredim_t0': df_haredim_t0, 'df_haredim_t1': df_haredim_t1,
+                'df_seniors_t0': df_seniors_t0, 'df_seniors_t1': df_seniors_t1,
+                'df_holocaust_t0': df_holocaust_t0, 'df_holocaust_t1': df_holocaust_t1}
+
+
+    df_main_dash_out = create_df_main_dash(dfs_dict)
     print('hi')
 
 
