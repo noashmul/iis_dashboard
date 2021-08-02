3f4dbd14c1e8a26d297b99091ef3415e6c79493d
yotammarton <yonam92@gmail.com>
2021-08-02 20:56:40 +0300

    Yotam: Add utils (annotations)

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ def add_annotations_to_fig(fig, x, y, percentage_change_value):
     :param percentage_change_value: the text values to annotate
     """
     up, down = "\U000025B2", "\U000025BC"
-    percentage_change_value = [round(val) for val in percentage_change_value]
+    percentage_change_value = [round(val, 1) for val in percentage_change_value]
     text = [f'{up} {val}%' if val > 0 else f'{down} {val}%' for val in percentage_change_value]
 
     fig.add_trace(go.Scatter(
@@ -27,14 +27,15 @@ def add_annotations_to_fig(fig, x, y, percentage_change_value):
             family="sans serif",
             size=18,
             color="black"
-        )
+        ),
+        name=''
     ))
 
 
 if __name__ == "__main__":
     """Test the annotation function"""
     df = px.data.gapminder().query("continent == 'Europe' and year == 2007 and pop > 2.e6")
-    fig = px.bar(df, y='pop', x=list(range(len(df))))
-    add_annotations_to_fig(fig=fig, x=list(range(len(df))), y=list(df['pop']),
-                           percentage_change_value=[100 * np.random.randn() for i in range(len(df))])
-    fig.show()
+    fig = px.bar(df, y='pop', x='country')
+    # add_annotations_to_fig(fig=fig, x=list(range(len(df))), y=list(df['pop']),
+    #                        percentage_change_value=[100 * np.random.randn() for i in range(len(df))])
+    # fig.show()
