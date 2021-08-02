c8c8ecda5c14d6851a48f0a5d94e22b91361cc5a
ggoldste <gal.goldstein@intel.com>
2021-08-02 18:36:20 +0300

    add elderly to app.py

---
+++ apps/elderly.py
@@ -0,0 +1,226 @@
+import dash
+import numpy as np
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import plotly.express as px
+from choroplethmapbox import get_choroplethmap_fig
+import folium
+import dash_table as dt
+from app_def import app
+from pre_process import *
+
+statistic_area = {'הכל': 0,
+                  'גן הבהאים': 612,
+                  'הדר מזרח - גאולה': 642,
+                  'הדר מזרח - רח\' יל"ג': 641,
+                  "הדר מערב - רח' אלמותנבי": 611,
+                  "הדר מערב - רח' מסדה": 613,
+                  'הדר מרכז - בית העירייה': 633,
+                  'הדר מרכז - התיאטרון העירוני': 631,
+                  "הדר מרכז - רח' הרצליה": 632,
+                  'הדר מרכז - שוק תלפיות': 634,
+                  "הדר עליון - רח' הפועל": 622,
+                  'הדר עליון -בי"ח בני ציון': 621,
+                  'מעונות גאולה': 644,
+                  "רמת הדר - רח' המיימוני": 623,
+                  "רמת ויז'ניץ": 643}
+
+options = list()
+for key, value in statistic_area.items():
+    if key != 'הכל':
+        options.append({'label': key + ' ' + str(value),
+                        'value': value})
+    else:
+        options.append({'label': key,
+                        'value': value})
+
+stat_zones_names_dict = {
+    611: "הדר מערב - רח' אלמותנבי",
+    612: 'גן הבהאים',
+    613: "הדר מערב - רח' מסדה",
+    621: 'הדר עליון -בי"ח בני ציון',
+    622: "הדר עליון - רח' הפועל",
+    623: "רמת הדר - רח' המיימוני",
+    631: 'הדר מרכז - התיאטרון העירוני',
+    632: "הדר מרכז - רח' הרצליה",
+    633: 'הדר מרכז - בית העירייה',
+    634: 'הדר מרכז - שוק תלפיות',
+    641: 'הדר מזרח - רח\' יל"ג',
+    642: 'הדר מזרח - גאולה',
+    643: "רמת ויז'ניץ",
+    644: 'מעונות גאולה'
+}
+
+# Calculate % change in total amount of seniors from time t0 to t1
+seniors0, seniors1 = dfs_dict['df_seniors_t0'], dfs_dict['df_seniors_t1']
+for df in [seniors0, seniors1]:
+    df['total_crime_cases'] = df[[col for col in df.columns if col != 'StatZone' and col != 'Year']].sum(axis=1)
+
+# percentage change in % units from time0 to time1
+percentage_change = 100 * (crime1.total_crime_cases - crime0.total_crime_cases) / crime0.total_crime_cases
+values_for_heatmap = {statzone_code: perc_change for statzone_code, perc_change in
+                      zip(stat_zones_names_dict.keys(), percentage_change)}
+
+map_fig = get_choroplethmap_fig(values_dict={k: int(v) for k, v in values_for_heatmap.items()},
+                                map_title="% of change in total crime cases",
+                                colorscale=[[0, '#561162'], [0.5, 'white'], [1, '#0B3B70']],
+                                hovertemplate='<b>StatZone</b>: %{text}' + '<br><b>Precentage of change</b>: %{z}%<br>')
+map_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+
+@app.callback(
+    Output(component_id='crime_trend_graph', component_property='figure'),
+    Output(component_id='crime_location_type', component_property='figure'),
+    Output(component_id='crime_type', component_property='figure'),
+    Input(component_id='areas', component_property='value')
+)
+def get_graphs(statzone):
+    df_crimes = dfs_dict['df_crime_2010_to_2015']
+    df_crimes = df_crimes[df_crimes['Year'] == 2012]
+    if statzone == 0:
+        statzone = 'All Statistical zones'
+
+    if statzone == 'All Statistical zones':
+        df_total_crimes = df_crimes.groupby(by=["Month"]).count()[['Street']] / 14
+        df_total_crime_all = pd.DataFrame(columns=['Month', 'Amount of Crimes'])
+        df_total_crime_all['Month'] = df_total_crimes['Street'].index
+        df_total_crime_all['Amount of Crimes'] = df_total_crimes['Street'].values
+        fig1 = px.line(df_total_crime_all, x=df_total_crime_all['Month']
+                       , y=df_total_crime_all['Amount of Crimes'], color_discrete_sequence=['#252E3F'])
+        max_y = df_total_crime_all['Amount of Crimes'].max()
+    else:
+        df_total_crimes = df_crimes.groupby(by=["StatZone", "Month"]).count()[['Street']]
+        df_tot_crime_per_area = pd.DataFrame(columns=['Month', 'Amount of Crimes'])
+        df_tot_crime_per_area['Month'] = df_total_crimes.loc[statzone]['Street'].index
+        df_tot_crime_per_area['Amount of Crimes'] = df_total_crimes.loc[statzone]['Street'].values
+        fig1 = px.line(df_tot_crime_per_area, x=df_tot_crime_per_area['Month']
+                       , y=df_tot_crime_per_area['Amount of Crimes'], color_discrete_sequence=['#252E3F'])
+        max_y = df_tot_crime_per_area['Amount of Crimes'].max()
+
+    string = " stat zone " if statzone != 'All Statistical zones' else " "
+
+    # TODO this title is not visible because of the margin 0, add a title to the html
+    fig1.update_layout(title_text=f"Amount of crimes per month in{string}{statzone}",
+                       yaxis=dict(
+                           titlefont_size=18,
+                           tickfont_size=18,
+                       ),
+                       xaxis=dict(
+                           titlefont_size=18,
+                           tickfont_size=18,
+                           tickvals=[i for i in range(13)],
+                           ticktext=[i for i in range(13)]
+                       ), xaxis_showgrid=True, yaxis_showgrid=True,
+                       template='none',
+                       yaxis_range=[0, max_y * 1.1])
+
+    if statzone == 'All Statistical zones':
+        df_location = df_crimes.groupby(by=["CrimeLocType"]).count()[['Street']] / 14
+        df_location = df_location.sort_values(by=['Street'], ascending=False).head(10)
+        df_location.index = [s[::-1].strip(' ') for s in df_location.index]
+        df_location.rename(columns={'Street': 'Amount of Crimes'}, inplace=True)
+    else:
+        df_location = df_crimes.groupby(by=["StatZone", "CrimeLocType"]).count()[['Street']]
+        df_location = df_location.loc[statzone].sort_values(by=['Street'], ascending=False).head(10)
+        df_location.index = [s[::-1].strip(' ') for s in df_location.index]
+        df_location.rename(columns={'Street': 'Amount of Crimes'}, inplace=True)
+
+    # TODO the next two graphs are not centered and are being cut from the bottom
+    fig2 = px.bar(df_location, x=df_location.index, y=df_location['Amount of Crimes'],
+                  color_discrete_sequence=['#252E3F'])
+    # for_title = "crimes per location" if graph_type == "CrimeLocType" else "crimes per type"
+    fig2.update_layout(title_text=f"Amount of crimes per location in<br> {string}{statzone}",
+                       yaxis=dict(
+                           titlefont_size=14,
+                           tickfont_size=14,
+                       ),
+                       xaxis=dict(
+                           titlefont_size=14,
+                           tickfont_size=14,
+                       ), xaxis_showgrid=True, yaxis_showgrid=True, template='none')
+    fig2.update_xaxes(title='Crime location', tickangle=45)
+
+    if statzone == 'All Statistical zones':
+        df_type = df_crimes.groupby(by=["CrimeType"]).count()[['Street']] / 14
+        df_type = df_type.sort_values(by=['Street'], ascending=False).head(10)
+        df_type.index = [s[::-1].strip(' ') for s in df_type.index]
+        df_type.rename(columns={'Street': 'Amount of Crimes'}, inplace=True)
+    else:
+        df_type = df_crimes.groupby(by=["StatZone", "CrimeType"]).count()[['Street']]
+        df_type = df_type.loc[statzone].sort_values(by=['Street'], ascending=False).head(10)
+        df_type.index = [s[::-1].strip(' ') for s in df_type.index]
+        df_type.rename(columns={'Street': 'Amount of Crimes'}, inplace=True)
+
+    fig3 = px.bar(df_type, x=df_type.index, y=df_type['Amount of Crimes'],
+                  color_discrete_sequence=['#252E3F'])
+    fig3.update_layout(title_text=f"Amount of crimes per type in<br> {string}{statzone}",
+                       yaxis=dict(
+                           titlefont_size=14,
+                           tickfont_size=14,
+                       ),
+                       xaxis=dict(
+                           titlefont_size=14,
+                           tickfont_size=14,
+                       ), xaxis_showgrid=True, yaxis_showgrid=True,
+                       template='none',
+                       )
+    fig3.update_xaxes(title='Crime type', tickangle=45)
+
+    return fig1, fig2, fig3
+
+
+layout = html.Div(
+    children=[
+        html.H4(children='Choose the wanted area to see the graphs changes',  # TODO adjust title?
+                style={'text-align': 'left', 'text-transform': 'none', 'font-family': 'sans-serif',
+                       'letter-spacing': '0em'}
+                ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        'Choose area: ', dcc.RadioItems(id='areas',
+                                                        options=options,
+                                                        value=0
+                                                        ),
+                    ],
+                    className="mini_container",
+                ),
+                html.Div([
+                    dcc.Graph(figure=map_fig)
+                ], className="map_container"),
+            ],
+            id="info-container1",
+            className="row container-display",
+        ),
+        html.Div(
+            [
+                dcc.Graph(id='crime_trend_graph')
+            ], className="graph_container"
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Graph(id='crime_location_type')
+                    ],
+                    className='narrow_container',
+                ),
+                html.Div(
+                    [
+                        dcc.Graph(id='crime_type')
+                    ],
+                    className='narrow_container',
+                ),
+            ],
+            id="info-container2",
+            className="row container-display",
+        )
+    ],
+    className="pretty_container twelve columns",
+    style={"text-align": "justify"},
+)
