3b5d92e651751b47dd99214d47a64e70e78b1179
yotammarton <yonam92@gmail.com>
2021-08-05 21:27:57 +0300

    Yotam: Updates

--- apps/population.py
+++ apps/population.py
@@ -102,17 +102,18 @@ def get_graphs(statzone):
                                                    ignore_index=True)
         age_group_df_old = age_group_df_old[age_group_df_old['StatZone'] == 'All']
 
-        title1 = 'Haredim percentage in All Statistical zones'
-        title2 = 'Genders percentages in All Statistical zones'
-        title3 = 'Age groups distribution in All Statistical zones'
+        title1 = 'אחוז חרדים בכל שכונת הדר'[::-1]
+        title2 = 'אחוז נשים וגברים בכל שכונת הדר'[::-1]
+        title3 = 'התפלגות גילאים בכל שכונת הדר'[::-1]
+
     else:
         haredim_df = haredim_df[haredim_df['StatZone'] == statzone]
         gender_df = gender_df[gender_df['StatZone'] == statzone]
         age_group_df = age_group_df[age_group_df['StatZone'] == statzone]
         age_group_df_old = age_group_df_old[age_group_df_old['StatZone'] == statzone]
-        title1 = f'Haredim percentage in {statzone} stat zone'
-        title2 = f'Genders percentages in {statzone} stat zone'
-        title3 = f'Age groups distribution in {statzone} stat zone'
+        title1 = f'אחוז חרדים באזור סטטיסטי {str(statzone)[::-1]} '[::-1]
+        title2 = f'אחוז נשים וגברים באזור סטטיסטי {str(statzone)[::-1]} '[::-1]
+        title3 = f'התפלגות גילאים באזור סטטיסטי {str(statzone)[::-1]} '[::-1]
 
     pie1_df = {'Label': ['חרדים', 'לא חרדים'],
                'Amount': [int(haredim_df['TotHaredim']), int(haredim_df['Total']) - int(haredim_df['TotHaredim'])]}
@@ -120,13 +121,14 @@ def get_graphs(statzone):
 
     fig1 = px.pie(pie1_df, values='Amount', names='Label', title=title1, color='Label',
                   color_discrete_map={list(pie1_df['Label'])[0]: '#8FBC8F', list(pie1_df['Label'])[1]: '#F5DEB3'})
-
+    fig1.update_layout(title_x=0.5, )
     pie2_df = {'Gender': ['גברים', 'נשים'],
                'Amount': [int(gender_df['Men']), int(gender_df['Total']) - int(gender_df['Men'])]}
     pie2_df['Gender'] = [(str(x)[::-1]) for x in pie2_df['Gender']]
 
     fig2 = px.pie(pie2_df, values='Amount', names='Gender', title=title2, color='Gender',
                   color_discrete_map={list(pie2_df['Gender'])[0]: 'skyblue', list(pie2_df['Gender'])[1]: 'lightpink'})
+    fig2.update_layout(title_x=0.5, )
     age_group_df_new = pd.DataFrame(columns=['Age group', 'Amount of citizen'])
     age_group_df_old_new = pd.DataFrame(columns=['Age group', 'Amount of citizen'])
     old_y = []
@@ -148,7 +150,10 @@ def get_graphs(statzone):
 
     fig3 = px.bar(age_group_df_new, y=age_group_df_new['Amount of citizen'],
                   x=age_group_df_new['Age group'], title=title3, color_discrete_sequence=['#252E3F'])
-    fig3.update_layout(title_text=title3,
+    fig3.update_yaxes(title="כמות תושבים"[::-1])
+    fig3.update_xaxes(title="קבוצת גיל"[::-1])
+    fig3.update_layout(title_text=title3, title_x=0.5,
+
                        yaxis=dict(
                            titlefont_size=18,
                            tickfont_size=18,
@@ -156,12 +161,12 @@ def get_graphs(statzone):
                        xaxis=dict(
                            titlefont_size=18,
                            tickfont_size=18,
-                       ), xaxis_showgrid=True, yaxis_showgrid=True)
+                       ), xaxis_showgrid=True, yaxis_showgrid=True,
+                       template='simple_white')
     add_annotations_to_fig(fig3, fig3.data[0].x, fig3.data[0].y, percentage_change, old_y=old_y)
     fig3.update_layout(showlegend=False)
     max_y = max(age_group_df_new['Amount of citizen'])
     fig3.update_layout(yaxis_range=[0, max_y * 1.1])
-
     return fig1, fig2, fig3
 
 
@@ -193,10 +198,18 @@ def change_map(map_def):
 
 
 layout = html.Div(children=[
-    html.H4(children='Choose the wanted area to see the graphs changes',
-            style={'text-align': 'left', 'text-transform': 'none', 'font-family': 'sans-serif',
-                   'letter-spacing': '0em'}, className="pretty_container"
-            ),
+    html.H4(
+        children=[html.H4(['בעמוד הנוכחי תוכלו לראות מגוון נתונים בנושא הדמוגרפיה של הדר'],
+                          style={'text-align': 'right', 'text-transform': 'none', 'font-family': 'sans-serif',
+                                 'letter-spacing': '0em'}, ),
+                  html.H4([
+                      'ניתן לבחור ולראות מפה המציגה את הערכים הנוכחיים של כמות האוכלוסיה הכללית בכל אזור סטטיסטי, וכן מפה המראה את השינויים מהחצי שנה הקודמת. בהמשך מוצגים גרפים אשר מציגים מידע נוסף על אוכלוסיה זו, וניתן לבחור להציג בהם מידע רק על אזור סטטיסטי מסוים. מוצגת למטה החלוקה של האוכלוסיה לנשים / גברים ולאחוז החרדים (עבור הדר כולה או עבור האזור הנבחר - במעבר על גרף העוגה ניתן לראות גם את הערכים עצמם ולא רק אחזוים). בנוסף, מוצג גרף אשר מראה את התפלגות הגילאים ואת השינוי מהחצי שנה הקודמת']
+                      , style={'text-align': 'right', 'text-transform': 'none', 'font-family': 'sans-serif',
+                               'letter-spacing': '0em', 'line-height': '1.6em'}
+                  )]
+        ,
+        className='pretty_container'
+    ),
     html.Div([
         html.Div([html.H6('בחר את תצוגת המפה', style={'Font-weight': 'bold', 'text-transform': 'none',
                                                       'letter-spacing': '0em', 'font-family': 'sans-serif',
@@ -215,9 +228,9 @@ layout = html.Div(children=[
                 html.Div(
                     [
                         ': בחר אזור', dcc.RadioItems(id='areas',
-                                                        options=options,
-                                                        value=0,
-                                                        ),
+                                                     options=options,
+                                                     value=0,
+                                                     ),
                     ],
                     className="mini_container",
                 ),
